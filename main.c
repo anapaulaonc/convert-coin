55439683421dde32353aa098859a8a905a59f107
Ana Paula Nóbrega <54750948+anapaulaonc@users.noreply.github.com>
2025-07-15 00:01:54 -0300

    conversions complete to both sides

--- main.c
+++ main.c
@@ -102,89 +102,114 @@ uint8_t readADC(uint8_t channel) {
     return ADC12MEM0 & 0xFF;
 }
 
-void chooseCoin(int step) {
-    switch(step){
+void showCurrency(int coin, char* prefix) {
+    lcdWrite(prefix);
+    switch(coin){
         case 1:
-            lcdWrite("Dolar\n");
+            lcdWrite("BRL (Real)\n");
             break;
         case 2:
-            lcdWrite("Euro\n");
+            lcdWrite("USD (Dolar)\n");
             break;
         case 3:
-            lcdWrite("BitCoin\n");
+            lcdWrite("EUR (Euro)\n");
             break;
         case 4:
-            lcdWrite("Yen\n");
+            lcdWrite("BTC (Bitcoin)\n");
+            break;
+        case 5:
+            lcdWrite("JPY (Yen)\n");
             break;
     }
 }
 
-void calculateConversion(int coin, long amountCents, int* intPart, int* decPart) {
+char* getCurrencySymbol(int coin) {
+    switch(coin) {
+        case 1: return "BRL";
+        case 2: return "USD";
+        case 3: return "EUR";
+        case 4: return "BTC";
+        case 5: return "JPY";
+        default: return "???";
+    }
+}
+
+long convertToBase(int fromCoin, long amountCents) {
+    switch(fromCoin) {
+        case 1: // BRL
+            return amountCents;
+        case 2: // USD
+            return (amountCents * 500L) / 100L;
+        case 3: // EUR
+            return (amountCents * 555L) / 100L;
+        case 4: // BTC (satoshis para BRL)
+            return amountCents * 20000L;
+        case 5: // JPY
+            return (amountCents * 100L) / 3000L;
+        default:
+            return amountCents;
+    }
+}
+
+void convertFromBase(int toCoin, long baseCents, int* intPart, int* decPart) {
     long result;
     
-    switch(coin) {
-        case 1: // Dólar
-            result = (amountCents * 18L) / 100L;
+    switch(toCoin) {
+        case 1: // BRL
+            result = baseCents;
             break;
-        case 2: // Euro
-            result = (amountCents * 15L) / 100L;
+        case 2: // USD
+            result = (baseCents * 100L) / 500L;
             break;
-        case 3: // Bitcoin
-            result = amountCents / 20000L;
-            if (result == 0 && amountCents > 0) result = 1;
+        case 3: // EUR
+            result = (baseCents * 100L) / 555L;
+            break;
+        case 4: // BTC
+            result = baseCents / 20000L;
+            if (result == 0 && baseCents > 0) result = 1;
             *intPart = (int)result;
             *decPart = 0;
             return;
-        case 4: 
-            result = (amountCents * 2600L) / 100L;
+        case 5: // JPY
+            result = (baseCents * 3000L) / 100L;
             break;
         default:
-            result = amountCents;
+            result = baseCents;
     }
     
     *intPart = (int)(result / 100L);
     *decPart = (int)(result % 100L);
 }
 
-void showResult(int coin, long amountCents, int intPart, int decPart) {
+void showResult(int fromCoin, int toCoin, long amountCents, int intPart, int decPart) {
     char buffer[32];
-
+    char* fromSymbol = getCurrencySymbol(fromCoin);
+    char* toSymbol = getCurrencySymbol(toCoin);
+    
     lcdClear();
-
-    int brlInt = (int)(amountCents / 100L);
-    int brlDec = (int)(amountCents % 100L);
-    if (brlDec < 10) {
-        sprintf(buffer, "BRL %d.0%d = \n", brlInt, brlDec);
+    
+    int fromInt = (int)(amountCents / 100L);
+    int fromDec = (int)(amountCents % 100L);
+    
+    if (fromCoin == 4) {
+        sprintf(buffer, "%s %d = \n", fromSymbol, fromInt);
     } else {
-        sprintf(buffer, "BRL %d.%d = \n", brlInt, brlDec);
+        if (fromDec < 10) {
+            sprintf(buffer, "%s %d.0%d = \n", fromSymbol, fromInt, fromDec);
+        } else {
+            sprintf(buffer, "%s %d.%d = \n", fromSymbol, fromInt, fromDec);
+        }
     }
     lcdWrite(buffer);
-
-    switch(coin) {
-        case 1:
-            if (decPart < 10) {
-                sprintf(buffer, "USD %d.0%d\n", intPart, decPart);
-            } else {
-                sprintf(buffer, "USD %d.%d\n", intPart, decPart);
-            }
-            break;
-        case 2:
-            if (decPart < 10) {
-                sprintf(buffer, "EUR %d.0%d\n", intPart, decPart);
-            } else {
-                sprintf(buffer, "EUR %d.%d\n", intPart, decPart);
-            }
-            break;
-        case 3:
-            sprintf(buffer, "BTC %d sat\n", intPart);
-            break;
-        case 4:
-            if (decPart < 10) {
-                sprintf(buffer, "JPY %d.0%d\n", intPart, decPart);
-            } else {
-                sprintf(buffer, "JPY %d.%d\n", intPart, decPart);
-            }
-            break;
+    
+    if (toCoin == 4) {
+        sprintf(buffer, "%s %d\n", toSymbol, intPart);
+    } else {
+        if (decPart < 10) {
+            sprintf(buffer, "%s %d.0%d\n", toSymbol, intPart, decPart);
+        } else {
+            sprintf(buffer, "%s %d.%d\n", toSymbol, intPart, decPart);
+        }
     }
     lcdWrite(buffer);
 }
@@ -197,8 +222,8 @@ void main(void) {
     buttonConfig();
     lcdInit();
 
-    unsigned int coin = 1;
-    unsigned int selected = 0;
+    unsigned int fromCoin = 1;
+    unsigned int toCoin = 2;
     unsigned int fase = 0;
 
     unsigned int lastXDir = 0;
@@ -211,8 +236,26 @@ void main(void) {
     int convertedInt = 0;
     int convertedDec = 0;
 
-    lcdWrite("Selecione >>\n");
-    lcdWrite("a moeda:  >>\n");
+    lcdClear();
+    lcdWrite("COTACAO LIVE\n");
+   
+    __delay_cycles(3000000);
+    
+    lcdClear();
+    lcdWrite("5 Moedas\n");
+    lcdWrite("Aperte botao...");
+    
+    while (1) {
+        unsigned int buttonState = P2IN & BIT5;
+        if (buttonState == 0 && lastButtonState != 0) {
+            break;
+        }
+        lastButtonState = buttonState;
+        __delay_cycles(50000);
+    }
+    
+    lcdClear();
+    showCurrency(fromCoin, "De: ");
 
     while (1) {
         unsigned int xValue = readADC(ADC12INCH_0);
@@ -224,21 +267,39 @@ void main(void) {
             if (xValue > 110 && xValue < 140) {
                 lastXDir = 0;
             } else if (xValue >= 180 && lastXDir != 1) {
-                if (coin < 4) coin++;
+                if (fromCoin < 5) fromCoin++;
                 lcdClear();
-                chooseCoin(coin);
+                showCurrency(fromCoin, "De: ");
                 lastXDir = 1;
                 __delay_cycles(200000);
             } else if (xValue <= 50 && lastXDir != 2) {
-                if (coin > 1) coin--;
+                if (fromCoin > 1) fromCoin--;
                 lcdClear();
-                chooseCoin(coin);
+                showCurrency(fromCoin, "De: ");
                 lastXDir = 2;
                 __delay_cycles(200000);
             }
         }
 
         if (fase == 1) {
+            if (xValue > 110 && xValue < 140) {
+                lastXDir = 0;
+            } else if (xValue >= 180 && lastXDir != 1) {
+                if (toCoin < 5) toCoin++;
+                lcdClear();
+                showCurrency(toCoin, "Para: ");
+                lastXDir = 1;
+                __delay_cycles(200000);
+            } else if (xValue <= 50 && lastXDir != 2) {
+                if (toCoin > 1) toCoin--;
+                lcdClear();
+                showCurrency(toCoin, "Para: ");
+                lastXDir = 2;
+                __delay_cycles(200000);
+            }
+        }
+
+        if (fase == 2) {
             if (xValue > 110 && xValue < 140) {
                 lastXDir = 0;
             } else if (xValue >= 180 && lastXDir != 1) {
@@ -270,14 +331,24 @@ void main(void) {
             if (precisaAtualizarLCD) {
                 lcdClear();
                 char buffer[16];
-                sprintf(buffer, "%d%d%d.%d%d BRL", valor[0], valor[1], valor[2], valor[3], valor[4]);
+                char* symbol = getCurrencySymbol(fromCoin);
+                
+                if (fromCoin == 4) {
+                    sprintf(buffer, "%d%d%d%d%d %s", valor[0], valor[1], valor[2], valor[3], valor[4], symbol);
+                } else {
+                    sprintf(buffer, "%d%d%d.%d%d %s", valor[0], valor[1], valor[2], valor[3], valor[4], symbol);
+                }
                 lcdWrite(buffer);
 
                 char cursorLine[17] = "                ";
-                if (cursorPos < 3)
+                if (fromCoin == 4) {
                     cursorLine[cursorPos] = '^';
-                else
-                    cursorLine[cursorPos + 1] = '^';
+                } else {
+                    if (cursorPos < 3)
+                        cursorLine[cursorPos] = '^';
+                    else
+                        cursorLine[cursorPos + 1] = '^';
+                }
 
                 lcdWriteByte(0xC0, 0);
                 lcdWrite(cursorLine);
@@ -289,31 +360,51 @@ void main(void) {
         unsigned int buttonState = P2IN & BIT5;
         if (buttonState == 0 && lastButtonState != 0) {
             if (fase == 0) {
-                selected = coin;
                 fase = 1;
+                if (toCoin == fromCoin) toCoin = (fromCoin == 1) ? 2 : 1;
+                lcdClear();
+                showCurrency(toCoin, "Para: ");
+                
+            } else if (fase == 1) {
+                fase = 2;
                 valor[0] = valor[1] = valor[2] = valor[3] = valor[4] = 0;
                 cursorPos = 0;
                 lcdClear();
                 char buffer[16];
-                sprintf(buffer, "%d%d%d.%d%d BRL", valor[0], valor[1], valor[2], valor[3], valor[4]);
+                char* symbol = getCurrencySymbol(fromCoin);
+                
+                if (fromCoin == 4) {
+                    sprintf(buffer, "%d%d%d%d%d %s", valor[0], valor[1], valor[2], valor[3], valor[4], symbol);
+                } else {
+                    sprintf(buffer, "%d%d%d.%d%d %s", valor[0], valor[1], valor[2], valor[3], valor[4], symbol);
+                }
                 lcdWrite(buffer);
+                
                 char cursorLine[17] = "^               ";
                 lcdWriteByte(0xC0, 0);
                 lcdWrite(cursorLine);
-            } else if (fase == 1) {
-                long amountCents = (long)valor[0]*10000L + (long)valor[1]*1000L + (long)valor[2]*100L + (long)valor[3]*10L + (long)valor[4];
-                calculateConversion(selected, amountCents, &convertedInt, &convertedDec);
-                showResult(selected, amountCents, convertedInt, convertedDec);
-                fase = 2;
+                
             } else if (fase == 2) {
+                long amountCents;
+                if (fromCoin == 4) {
+                    amountCents = (long)valor[0]*10000L + (long)valor[1]*1000L + (long)valor[2]*100L + (long)valor[3]*10L + (long)valor[4];
+                } else {
+                    amountCents = (long)valor[0]*10000L + (long)valor[1]*1000L + (long)valor[2]*100L + (long)valor[3]*10L + (long)valor[4];
+                }
+                
+                long baseCents = convertToBase(fromCoin, amountCents);
+                convertFromBase(toCoin, baseCents, &convertedInt, &convertedDec);
+                showResult(fromCoin, toCoin, amountCents, convertedInt, convertedDec);
+                fase = 3;
+                
+            } else if (fase == 3) {
                 fase = 0;
-                coin = 1;
-                selected = 0;
+                fromCoin = 1;
+                toCoin = 2;
                 valor[0] = valor[1] = valor[2] = valor[3] = valor[4] = 0;
                 cursorPos = 0;
                 lcdClear();
-                lcdWrite("Selecione >>\n");
-                lcdWrite("a moeda:  >>\n");
+                showCurrency(fromCoin, "De: ");
             }
 
             __delay_cycles(300000);
